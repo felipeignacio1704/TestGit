a1021e3146b2f34e3f41e90a2c1e9daf78938c51
Felipe Naranjo <felipe.trabajos326@gmail.com>
2025-06-29 03:21:17 -0400

    Agregando funciones

--- test4.py
+++ test4.py
@@ -1,1 +1,10 @@
 print("Creado en local")
+
+def suma( a,  b):
+    resultado = a + b
+    return resultado
+
+a =int(input("Inserta un numero: "))
+b =int(input("Inserta un numero: "))
+print(suma(a,b))
+    
